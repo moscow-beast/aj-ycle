e8f0ab934c0d986bf02b7b245ab3853c98ac6121
Juriy Bodunov-Skvortsov <renard.ruse@gmail.com>
2016-09-27 12:09:35 +0300

    Add support for serializing of recursive objects for url encoding

--- ajcycle.js
+++ ajcycle.js
@@ -38,12 +38,16 @@ function ajсycle(props, values, callback) {
             }
             return xmlhttp;
         },
-        serializeObject: function (obj) {
+        serializeObject: function (obj,prefix) {
             var str = [];
-            for (var p in obj)
+            for (var p in obj) {
                 if (obj.hasOwnProperty(p)) {
-                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                    var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
+                    str.push(typeof v === "object" ?
+                            serialize(v, k) :
+                            encodeURIComponent(k) + "=" + encodeURIComponent(v));
                 }
+            }
             return str.join("&");
         },
         prepareBodyForPost: function () {
